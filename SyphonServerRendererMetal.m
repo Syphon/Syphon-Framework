00ec9c3aab673b91afa0144e814a241a2997eb42
Tom Butterworth <bangnoise@gmail.com>
2023-01-03 20:44:51 +0000

    Split Metal shaders into their own file, don't compile them from source at runtime

--- SyphonServerRendererMetal.m
+++ SyphonServerRendererMetal.m
@@ -1,79 +1,7 @@
 #import "SyphonServerRendererMetal.h"
 #import <Metal/Metal.h>
 #include <simd/simd.h>
-
-typedef enum SYPHONVertexInputIndex
-{
-    SYPHONVertexInputIndexVertices     = 0,
-    SYPHONVertexInputIndexViewportSize =  1,
-} SYPHONVertexInputIndex;
-
-
-typedef enum SYPHONTextureIndex
-{
-    SYPHONTextureIndexZero = 0,
-} SYPHONTextureIndex;
-
-typedef struct
-{
-    vector_float2 position;
-    vector_float4 color;
-} SYPHONColorVertex;
-
-typedef struct
-{
-    vector_float2 position;
-    vector_float2 textureCoordinate;
-} SYPHONTextureVertex;
-
-NSString *types = @""
-"#include <simd/simd.h>\n"
-"typedef enum SyphonVertexInputIndex\n"
-"{"
-"    SYPHONVertexInputIndexVertices     = 0,\n"
-"    SYPHONVertexInputIndexViewportSize =  1,\n"
-"} SYPHONVertexInputIndex;\n"
-"typedef enum SyphonTextureIndex\n"
-"{"
-"    SYPHONTextureIndexZero = 0,\n"
-"} SYPHONTextureIndex;\n"
-"typedef struct\n"
-"{"
-"    vector_float2 position;\n"
-"    vector_float4 color;\n"
-"} SYPHONColorVertex;\n"
-"typedef struct\n"
-"{"
-"    vector_float2 position;\n"
-"    vector_float2 textureCoordinate;\n"
-"} SYPHONTextureVertex;\n";
-
-NSString *shaderCode = @""
-"#include <metal_stdlib>\n"
-"#include <simd/simd.h>\n"
-"using namespace metal;\n"
-"typedef struct\n"
-"{"
-"    float4 clipSpacePosition [[position]];\n"
-"    float4 color;\n"
-"    float2 textureCoordinate;\n"
-"} RasterizerData;\n"
-"vertex RasterizerData textureToScreenVertexShader(uint vertexID [[ vertex_id ]], constant SYPHONTextureVertex *vertexArray [[ buffer(SYPHONVertexInputIndexVertices) ]], constant vector_uint2 *viewportSizePointer  [[ buffer(SYPHONVertexInputIndexViewportSize) ]]){"
-"RasterizerData out;"
-"float2 pixelSpacePosition = vertexArray[vertexID].position.xy;"
-"float2 viewportSize = float2(*viewportSizePointer);"
-"out.clipSpacePosition.xy = pixelSpacePosition / (viewportSize / 2.0);"
-"out.clipSpacePosition.z = 0.0;"
-"out.clipSpacePosition.w = 1.0;"
-"out.textureCoordinate = vertexArray[vertexID].textureCoordinate;"
-"return out;"
-"}\n"
-
-"fragment float4 textureToScreenSamplingShader(RasterizerData in [[stage_in]], texture2d<half> colorTexture [[ texture(SYPHONTextureIndexZero) ]]) {"
-"    constexpr sampler textureSampler (mag_filter::nearest, min_filter::nearest);"
-"    const half4 colorSample = colorTexture.sample(textureSampler, in.textureCoordinate);"
-"    return float4(colorSample);"
-"}";
+#include "SyphonServerMetalTypes.h"
 
 @implementation SyphonServerRendererMetal
 {
@@ -92,13 +20,11 @@ NSString *shaderCode = @""
         _device = device;
 
         NSError *error = NULL;
-        NSString *code = [types stringByAppendingString:shaderCode];
-        MTLCompileOptions *compileOptions = [MTLCompileOptions new];
-        compileOptions.languageVersion = MTLLanguageVersion1_2;
-        id<MTLLibrary> defaultLibrary = [device newLibraryWithSource:code options:compileOptions error:&error];
-        if( error )
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        id<MTLLibrary> defaultLibrary = [device newDefaultLibraryWithBundle:bundle error:&error];
+        if(error)
         {
-            SYPHONLOG(@"METAL SHADER COMPILER ERROR:%@", error);
+            SYPHONLOG(@"Metal library could not be loaded:%@", error);
         }
         
         // Load the vertex/shader function from the library
