Syphon/Syphon-Framework

commit 1aa9df0ae94d01a0830561258f59704121352f1b

SyphonCFMessageSender - fix unsafe access to CFMessagePort between threads

SyphonMessageSender.m
@@ -32,6 +32,10 @@
 #import "SyphonCFMessageSender.h"
 //#import "SyphonMachMessageSender.h"
 
+@interface SyphonMessageSender ()
+@property (readwrite, atomic) BOOL isValid;
+@end
+
 @implementation SyphonMessageSender
 {
 @private
@@ -71,11 +75,6 @@
 	return _name;
 }
 
-- (BOOL)isValid
-{
-	return NO;
-}
-
 - (void)send:(id <NSCoding>)payload ofType:(uint32_t)type
 {
 	// subclasses override this
@@ -83,6 +82,7 @@
 
 - (void)invalidate
 {
+    self.isValid = NO;
 	if (_handler != nil)
 	{
 		_handler();

SyphonCFMessageSender.h
@@ -2,7 +2,7 @@
     SyphonCFMessageSender.h
     Syphon
 
-    Copyright 2010-2011 bangnoise (Tom Butterworth) & vade (Anton Marini).
+    Copyright 2010-2023 bangnoise (Tom Butterworth) & vade (Anton Marini).
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without

SyphonCFMessageSender.m
@@ -2,7 +2,7 @@
     SyphonCFMessageSender.m
     Syphon
 
-    Copyright 2010-2011 bangnoise (Tom Butterworth) & vade (Anton Marini).
+    Copyright 2010-2023 bangnoise (Tom Butterworth) & vade (Anton Marini).
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,6 @@
 @implementation SyphonCFMessageSender
 {
 @private
-    CFMessagePortRef _port;
     SyphonMessageQueue *_queue;
     SyphonDispatchSourceRef _dispatch;
 }
@@ -49,8 +48,8 @@
     self = [super initForName:name protocol:protocolName invalidationHandler:handler];
 	if (self)
 	{
-		_port = CFMessagePortCreateRemote(kCFAllocatorDefault, (CFStringRef)name);
-		if (_port == NULL)
+		CFMessagePortRef port = CFMessagePortCreateRemote(kCFAllocatorDefault, (CFStringRef)name);
+		if (port == NULL)
 		{
 			return nil;
 		}
@@ -58,7 +57,6 @@
 		_queue = [[SyphonMessageQueue alloc] init];
         _queue.userInfo = (__bridge void *)(self);
 		// local vars for block references, see note below
-		CFMessagePortRef port = _port;
 		SyphonMessageQueue *queue = _queue;
 		_dispatch = SyphonDispatchSourceCreate(^(){
 			
@@ -77,7 +75,6 @@
 				{
 					if (result == kCFMessagePortIsInvalid)
 					{
-						[(SyphonCFMessageSender *)queue.userInfo finishPort];
 						[(SyphonCFMessageSender *)queue.userInfo invalidate];
 						break;
 					}
@@ -97,31 +94,9 @@
 	return self;
 }
 
-- (void)finishPort
-{
-	_queue.userInfo = nil;
-	// our CFMessagePort will be released in the dispatch source's completion block
-	// we must stop referencing it now
-	bool result;
-	do {
-		void *old = _port;
-		result = OSAtomicCompareAndSwapPtrBarrier(old, NULL, (void **)&_port);
-	} while (!result);
-	do {
-		void *old = _dispatch;
-		result = OSAtomicCompareAndSwapPtrBarrier(old, NULL, (void **)&_dispatch);
-		if (result) SyphonDispatchSourceRelease(old);
-	} while (!result);
-}
-
 - (void)dealloc
 {
-	[self finishPort];
-}
-
-- (BOOL)isValid
-{
-	return (_port != NULL ? CFMessagePortIsValid(_port) : NO);
+    SyphonDispatchSourceRelease(_dispatch);
 }
 
 - (void)send:(id <NSCoding>)payload ofType:(uint32_t)type

commit 190a885ac80252c983b054247bb4bcd2969ce7f9

Fix issue which could cause other Syphon clients to stop updating after one client quit
Issue introduced by 1aa9df0ae94d01a0830561258f59704121352f1b

Fixes #102

SyphonMessageSender.m
@@ -64,6 +64,7 @@
 			// SyphonMessageSender init here
 			_handler = [handler copy];
 			_name = [name copy];
+            _isValid = YES;
 		}
 	}
 	return self;